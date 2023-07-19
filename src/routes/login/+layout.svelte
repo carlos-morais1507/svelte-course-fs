14aea39e138833805b1e8876e00488a27db42ebf
c.morais <c.morais1507@gmail.com>
2023-07-19 19:12:05 -0300

    :sparkles: feat: Multi-step Forms

--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
+    import { page } from "$app/stores";
 </script>
+
+<nav class="w-full flex items-center justify-center h-32">
+    <ul class="steps">
+        <a href="/login" class="step step-primary">Sign In</a>
+        <a href="/login/username" class="step" class:step-primary={$page.route.id?.match(/username|photo/g)} >Choose Username</a>
+        <a href="/login/photo" class="step" class:step-primary={$page.route.id?.match(/photo/g)}>Upload Photo</a>
+    </ul>
+</nav>
 
 <main class="card w-4/6 bg-neutral text-neutral-content mx-auto">
     <div class="card-body items-center text-center">
