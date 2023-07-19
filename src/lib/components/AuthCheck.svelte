c3cb34f9a8239e0a087d0b393511b762a013fbf6
c.morais <c.morais1507@gmail.com>
2023-07-19 20:10:49 -0300

    :white_check_mark: feat: Add AuthCheck

---
+++ src/lib/components/AuthCheck.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { user } from "$lib/firebase";
+</script>
+
+{#if $user}
+  <slot />
+{:else}
+  <p class="text-error">
+    You must be signed in to view this page.
+  </p>
+  <a class="btn btn-secondary" href="/login">Sign in</a>
+{/if}
--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -1,1 +1,14 @@
-<h2>Username</h2>
+<script lang="ts">
+  import AuthCheck from "$lib/components/AuthCheck.svelte";
+  import { db, user } from '$lib/firebase';
+  import { doc, getDoc, writeBatch } from "firebase/firestore";
+
+  let username = "";
+  let loading = false;
+  let isAvailable = false;
+
+</script>
+
+<AuthCheck>
+  <h2>Username</h2>
+</AuthCheck>
