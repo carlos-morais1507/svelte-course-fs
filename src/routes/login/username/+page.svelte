71b1117a291841ccd5d54f84bc4d911c07f61dff
c.morais <c.morais1507@gmail.com>
2023-07-19 20:43:01 -0300

    :sparkles: feat: Username Check

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -7,8 +7,41 @@
   let loading = false;
   let isAvailable = false;
 
+  let debounceTimer: NodeJS.Timeout;
+
+  async function checkAvailability() {
+    isAvailable = false;
+    clearTimeout(debounceTimer);
+    loading = true;
+
+    debounceTimer = setTimeout(async () => {
+      console.log("checking availability of ", username, "...");
+
+      const ref = doc(db, "usernames", username);
+      const exists = await getDoc(ref).then((doc) => doc.exists())
+
+      isAvailable = !exists;
+      loading = false;
+    }, 500)
+
+  }
+
+  async function confirmUsername() {}
+
 </script>
 
 <AuthCheck>
   <h2>Username</h2>
+  <form class="w-2/5" on:submit|preventDefault={confirmUsername}>
+    <input
+      type="text"
+      placeholder="Username"
+      class="input w-full"
+      bind:value={username}
+      on:input={checkAvailability}
+    />
+
+    <p class="mt-5">Is Available? <span class={isAvailable? "text-success": "text-error"}>{isAvailable}</span></p>
+    <button class="btn btn-success mt-3">Confirm username @{username}</button>
+  </form>
 </AuthCheck>
